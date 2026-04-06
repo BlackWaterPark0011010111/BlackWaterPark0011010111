f531d202b30aa14c4767720bf4a28d9f2226b3ba
BlackWater Park <blairdeleon@protonmail.com>
2026-04-06 21:46:22 +0200

    add basic

--- MySnake/Python/GSI Web_Page/design-pages/src/App.jsx
+++ MySnake/Python/GSI Web_Page/design-pages/src/App.jsx
@@ -226,8 +226,8 @@ useEffect(() => {
                 backgroundAttachment: 'scroll'  // параллакс-эффект (фон не скроллится)
                 // ← ИЗМЕНЯЙТЕ: 'fixed' на 'scroll' для обычного поведения
               }}
-            />
-            
+            /> 
+             
             {/* Тёмный градиент поверх фона для читаемости текста */}
             <div className="absolute inset-0 z-10 bg-gradient-to-b from-black/70 via-black/50 to-black/80"></div>
             {/* ← ИЗМЕНЯЙТЕ градиент: from-purple-900/70, to-black/90 и т.д. */}
